8b7c8dea34b0af44ceefc810d39bc0883c663329
yuhaowin <yuhao.coding@gmail.com>
2026-07-06 23:56:16 +0800

    init

---
+++ ch1/dup3/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	fileNames := os.Args[1:]
+	for _, filename := range fileNames {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
+}
