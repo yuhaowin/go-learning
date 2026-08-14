d6015101327dbea57d92fb098b632ccd9b57ba3e
yuhaowin <yuhao.coding@gmail.com>
2026-08-14 23:10:50 +0800

    refactor crawler

--- crawler/internal/engine/simple.go
+++ crawler/internal/engine/simple.go
@@ -1,11 +1,5 @@
 package engine
 
-import (
-	"log"
-
-	"github.com/yuhaowin/go-learning/crawler/internal/fetcher"
-)
-
 type SimpleEngine struct {
 	ItemChan chan any
 }
@@ -21,14 +15,10 @@ func (e SimpleEngine) Run(seeds ...Request) {
 		r := requests[0]
 		requests = requests[1:]
 
-		log.Printf("Fetching %s\n", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+		result, err := Worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
 			continue
 		}
-
-		result := r.ParserFunc(body)
 
 		for _, item := range result.Items {
 			go func() { e.ItemChan <- item }()
--- crawler/internal/engine/worker.go
+++ crawler/internal/engine/worker.go
@@ -8,6 +8,7 @@ import (
 
 // Worker 被并发执行
 func Worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s\n", r.Url)
 	body, err := fetcher.Fetch(r.Url)
 	if err != nil {
 		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
