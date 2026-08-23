aa6b41efddd936f8b19b869130afb88af514fc01
yuhaowin <yuhao.coding@gmail.com>
2026-08-24 01:02:16 +0800

    feat: implement byte counter, sleep program, and TCP clock server

---
+++ ch08/clock1/clock.go
@@ -0,0 +1,38 @@
+// Clock is a TCP server that periodically writes the time.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	// nc = netcat, a simple Unix utility which reads and writes data across network connections using TCP or UDP.
+	// nc localhost 8000
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		log.Println("accepted from", conn.RemoteAddr())
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
