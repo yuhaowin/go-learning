621beefd7673a6ab7094d9e6f57a3b9cf3926ad2
yuhaowin <yuhao.coding@gmail.com>
2026-08-29 01:26:18 +0800

    feat: implement countdown program with abort functionality

---
+++ ch08/countdown/version2/main.go
@@ -0,0 +1,29 @@
+// Countdown implements the countdown for a rocket launch.
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown.  Press return to abort.")
+	select {
+	case <-time.After(10 * time.Second):
+		launch()
+	case <-abort:
+		fmt.Println("Launch aborted!")
+		return
+	}
+}
+
+func launch() {
+	fmt.Println("Lift off!")
+}
